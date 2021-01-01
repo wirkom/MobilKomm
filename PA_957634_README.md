339e46a533eb3173d82cc570a6a1084ad8776f09
Mirko Wagner <wirkom@ludewig.fritz.box>
2021-01-01 21:21:45 +0100

    iadsf

--- PA_957634_README.md
+++ PA_957634_README.md
@@ -12,4 +12,4 @@ $ sh PA_957634_main.sh
 
 all traces are in PA_957634_helper, all plots are in PA_957634_plots.
 
-From past experiments I know, that my shell scripts often don't work on other shells (normally between my laptop and my pc in the uni some stuff breaks). I tested this on 3 systems. If there are still problems: Send me a mail (mirwagner@uos.de) and your config and I try my best to fix it.
+I do the plots in png, as gnuplot, for some reason, does not print the legend and the axes, when I use pdf instead of png. Afterwards all plots are put into one pdf via a tex document. If you do not have tex installed you can just look at the png version.
--- PA_957634_main.sh
+++ PA_957634_main.sh
@@ -22,23 +22,41 @@ rm -rf PA_957634_plots
 mkdir PA_957634_helper
 mkdir PA_957634_plots
 
+echo "waf build"
+./waf build
+
 # 2.1
+echo "2.1"
+echo "  friis"
 run 'f' '1' '351' '1' '0' '2_1' '2.1_3.1_4'
+echo "  tworay"
 run 't' '1' '351' '1' '0' '2_1' '2.1_3.1_4'
+echo "  logdistance"
 run 'l' '1' '351' '1' '0' '2_1' '2.1_3.1_4'
 
 # 2.2
+echo "2.2"
 run 'j' '0' '3601' '1' '10' '2_2' '2.2'
 
 # 3.1
+echo "3.1"
 run 'c' '1' '351' '1' '0' '3_1' '2.1_3.1_4'
 
 #3.2
+echo "3.2"
 run 'c' '5' '7100' '100' '0' '3_2' '3.2'
 
 # 4
+echo "4"
 run 's' '1' '351' '1' '0' '4' '2.1_3.1_4'
 
+echo "plots"
 gnuplot PA_957634_plot.plt
 gnuplot PA_957634_plot22.plt
 gnuplot PA_957634_plot32.plt
+
+echo "plots to pdf via latex"
+latexmk -pdf -interaction=nonstopmode PA_957634_plots_all.tex > PA_957634_plots_all_tex.log 2>&1
+mv PA_957634_plots_all.pdf PA_957634_plots
+mv PA_957634_plots_all.tex tmp
+mv tmp PA_957634_plots_all.tex
